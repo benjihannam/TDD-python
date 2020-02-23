eaeabaa2e70f1b723dc1cccabffa5c51213c7969
benjihannam <benjamin.c.hannam.18@dartmouth.edu>
2020-02-22 22:18:48 -0800

    running

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,6 @@
-from selenuim import webdriver
+from selenium import webdriver
 
-browser = webdriver.Chrome()
+browser = webdriver.Firefox()
 browser.get('http:localhost:8000')
 
 assert 'Django' in browser.title
